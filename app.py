5814da8f6f776a25ec4f959d94be3854a98b706d
aishwarya.bh <aishwab1@uci.edu>
2021-02-17 01:18:43 -0800

    added error class to organize errors

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash, jso
 from werkzeug.utils import secure_filename
 import os
 from sqlalchemy import between, and_
+from error import Error
 
 # local imports
 from config import db, app, UPLOAD_FOLDER, ALLOWED_EXTENSIONS
@@ -22,18 +23,18 @@ def post_bin_info():
     try:
         if request.method == 'POST':
             # Check if JSON request
-            if not request.json: return "Request body is not in JSON format", 400
+            if not request.json: raise Error("JSON_FORMAT")
 
             # Process request data
             post_data = request.json["data"]
             for row in post_data:
                 # bin_id, bin_height, location, bin_type, and waste_metrics must be present in body
                 if ("ip_address" not in row) or ("bin_height" not in row) or ("location" not in row) or ("bin_type" not in row) or ("waste_metrics" not in row):
-                    return "Missing key in body", 400
+                    raise Error("MISSING_KEY")
 
                 # bin_id, bin_height, location, bin_type, and waste_metrics cannot be null
                 if (row["ip_address"] is None) or (row["bin_height"] is None) or (row["location"] is None) or (row["bin_type"] is None) or (row["waste_metrics"] is None):
-                    return "Values cannot be null", 400
+                    raise Error("NULL_VALUE")
 
                 bin_data = models.BinInfo(ip_address=row["ip_address"],bin_height=row["bin_height"],\
                                    location=row["location"],bin_type=row["bin_type"],\
@@ -41,6 +42,8 @@ def post_bin_info():
                 db.session.add(bin_data)
                 db.session.commit()
             return "Posted: " + str(request.json), 201
+    except Error as e:
+        return Error.em[str(e)]
     except Exception as e:
         return str(e), 400
 
@@ -64,21 +67,21 @@ def post_usage():
     try:
         if request.method == 'POST':
             # Check if JSON request
-            if not request.json: return "Request body is not in JSON format", 400
+            if not request.json: raise Error("JSON_FORMAT")
             # Process request data
             post_data = request.json["data"]
             for row in post_data:
                 # bin_id and datetime must be present in body
                 if ("bin_id" not in row) or ("datetime" not in row):
-                    return "Missing key in body",
+                    raise Error("MISSING_KEY")
 
                 # bin_id and datetime cannot be null
                 if (row["bin_id"] is None) or (row["datetime"] is None):
-                    return "Values cannot be null", 400
+                   raise Error("NULL_VALUE")
 
                 # Check bin_id and return 400 if invalid bin_id
                 thebin = models.BinInfo.query.get(row["bin_id"])
-                if thebin is None: return "Invalid bin_id", 400
+                if thebin is None: raise Error("INVALID_BIN_ID")
 
                 # Add datetimestamp to db
                 usage_data = models.BinUsage(datetimestamp=row["datetime"], bin=thebin)
@@ -93,7 +96,7 @@ def post_usage():
 
             # None of the parameters can be null
             if bin_id is None or start_timestamp is None or end_timestamp is None:
-                return "Invalid parameter(s)", 400
+                raise Error("INVALID_PARAM")
             else:
                 # Get entries with same bin_id and between start_timestamp and end_timestamp
                 usage_data = models.BinUsage.query.filter(and_(models.BinUsage.bin_id == bin_id, between(models.BinUsage.datetimestamp, start_timestamp, end_timestamp))).all()
@@ -104,6 +107,8 @@ def post_usage():
                     ret["data"].append(dict(zip(keys,vals)))
                 return jsonify(ret), 200
 
+    except Error as e:
+        return Error.em[str(e)]
     except Exception as e:
         return str(e), 400
 
@@ -126,23 +131,23 @@ def post_weight():
     try:
         if request.method == 'POST':
             # Check if JSON request
-            if not request.json: return "Request body is not in JSON format", 400
+            if not request.json: raise Error("JSON_FORMAT")
 
             post_data = request.json["data"]
             for row in post_data:
 
                 # bin_id, datetime, and weight must be present in body
                 if ("bin_id" not in row) or ("datetime" not in row) or ("weight" not in row):
-                    return "Missing key in body", 400
+                    raise Error("MISSING_KEY")
 
                 # bin_id, datetime, or weight cannot be null
                 if (row["bin_id"] is None) or (row["datetime"] is None) or (row["weight"] is None):
-                    return "Values cannot be null", 400
+                    raise Error("NULL_VALUE")
 
                 # Check if bin_id and return 400 if invalid bin_id
                 thebin = models.BinInfo.query.get(row["bin_id"])
                 if thebin is None:
-                    return "Invalid bin_id", 400
+                    raise Error("INVALID_BIN_ID")
 
                 # Add weight data to db
                 weight_data = models.BinWeight(datetimestamp=row["datetime"], bin_weight=row["weight"], bin=thebin)
@@ -157,7 +162,7 @@ def post_weight():
 
             # None of the parameters can be null
             if bin_id is None or start_timestamp is None or end_timestamp is None:
-                return "Invalid parameter(s)", 400
+                raise Error("NULL_VALUE")
             else:
                 # Get entries with same bin_id and between start_timestamp and end_timestamp
                 weight_data = models.BinWeight.query.filter(and_(models.BinWeight.bin_id == bin_id, between(models.BinWeight.datetimestamp, start_timestamp, end_timestamp))).all()
@@ -167,6 +172,9 @@ def post_weight():
                     vals = [str(row.datetimestamp), row.bin_weight, row.bin_id]
                     ret["data"].append(dict(zip(keys,vals)))
                 return jsonify(ret), 200
+
+    except Error as e:
+        return Error.em[str(e)]
     except Exception as e:
         return str(e), 400
 
@@ -188,17 +196,17 @@ def get_weight_all():
 def get_fullness_info():
     try:
         if request.method == 'GET':
-            bin_id = request.args.get("bin_id")
+            bin_id = request.args.get("id")
             start_timestamp = request.args.get("start_timestamp")
             end_timestamp = request.args.get("end_timestamp")
 
             #throw error if any required parameters are None
-            if id == None or start_timestamp == None or end_timestamp == None:
-                return "bin_id, start timestamp, and end timestamp must be provided", 400
+            if bin_id == None or start_timestamp == None or end_timestamp == None:
+                raise Error("NULL_VALUE")
 
             #throw error if start_timestamp > end_timestamp
             if start_timestamp > end_timestamp:
-                return "start timestamp must be less than end timestamp", 400
+                raise Error("TIMESTAMP_ISSUE")
 
             bins = models.BinFullness.query.filter(and_(models.BinFullness.bin_id == bin_id, start_timestamp <= models.BinFullness.datetimestamp, end_timestamp >= models.BinFullness.datetimestamp)).all()
             ret = {"data":[]}
@@ -211,28 +219,31 @@ def get_fullness_info():
 
         elif request.method == 'POST':
             # Process request data
-            if not request.json: return "Request body is not in JSON format", 400
+            if not request.json: raise Error("JSON_FORMAT")
 
             post_data = request.json["data"]
 
             for row in post_data:
                 # bin_id, datetime, and fullness must be present in body
                 if ("bin_id" not in row) or ("datetime" not in row) or ("fullness" not in row):
-                    return "Missing key in body", 400
+                    raise Error("MISSING_KEY")
 
                 # bin_id, datetime, or weight cannot be null
                 if (row["bin_id"] is None) or (row["datetime"] is None) or (row["fullness"] is None):
-                    return "Values cannot be null", 400
+                    raise Error("NULL_VALUE")
 
                 # Check bin_id and return 400 if invalid bin_id
                 thebin = models.BinInfo.query.get(row["bin_id"])
-                if thebin is None: return "Invalid bin_id", 400
+                if thebin is None: raise Error("INVALID_BIN_ID")
 
                 # Add fullness to db
                 fullness_data = models.BinFullness(datetimestamp=row["datetime"],fullness=row["fullness"], bin=thebin)
                 db.session.add(fullness_data)
                 db.session.commit()
             return "Posted: " + str(request.json), 201
+
+    except Error as e:
+        return Error.em[str(e)]
 
     except Exception as e:
         return str(e), 400
@@ -259,7 +270,7 @@ def post_image():
     ip_addr = request.remote_addr
     if request.method == 'POST':
         if zbce_queries.ip_bin_id(ip_addr) == None:
-            return "Invalid Ip Address", 400
+            raise Error("INVALID_IP_ADDR")
         if 'file' not in request.files:
             flash('no file part in request')
             return redirect(request.url)
@@ -271,7 +282,7 @@ def post_image():
 
         img_path = UPLOAD_FOLDER + '/' + file.filename
         if os.path.isfile(img_path):
-            return "Image already exists", 400
+            raise Error("IMAGE_EXISTS")
 
         if file and allowed_file(file.filename):
             # extract the extension of the file
